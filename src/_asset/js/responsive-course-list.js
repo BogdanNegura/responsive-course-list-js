ff8c3cfcad1edfd61c390d7d013f96b4a1084fb0
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-03-11 20:55:01 +0000

    create the button Load more wich was selected by id

--- src/_asset/js/responsive-course-list.js
+++ src/_asset/js/responsive-course-list.js
@@ -2,21 +2,34 @@ try {
   const apiUrl = "https://learn.accountingcpd.net/ACPD/API/Test/SampleObject";
   const courseElement = document.querySelector("#course");
   const courseList = document.querySelector(".course__list");
+  const loadMore = document.querySelector("#course__load-more");
+  let limit = 10;
+  let result = [];
 
   fetch(apiUrl)
     // fix fetch response
     .then(r => r.json())
-    .then(d => render(d));
+    .then(d => {
+      result = d;
+      render();
+      console.log("sasa");
+    });
 
-  const render = result => {
+  const render = () => {
+    // show the load more button as long as we can load more courses
+    loadMore.className =
+      limit > result.length
+        ? "course__load-more--off"
+        : "course__load-more--on";
     courseElement.className = "list";
-
+    console.log(result);
     const courseListInnerHtml = result
+      .slice(0, limit)
       .map(elementInLoop => {
         return `<div class="course__card">
           <div class="course__type--${elementInLoop.type}" type="${elementInLoop.type}"></div>
-          <h1 class="course-title">${elementInLoop.title}</h1>
-          <p class="course-description">${elementInLoop.description}</p>
+          <h1 class="course__title">${elementInLoop.title}</h1>
+          <p class="course__description">${elementInLoop.description}</p>
           <p>Price: £${elementInLoop.price}</p>
         </div>`;
       })
